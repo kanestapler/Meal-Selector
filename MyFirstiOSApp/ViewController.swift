ea9d810375af197eb67bac09d0613001031fe259
Kane Stapler <kanestapler@gmail.com>
2017-08-27 21:51:00 -0400

    Initial Commit

---
+++ MyFirstiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstiOSApp
+//
+//  Created by Kane Stapler on 8/27/17.
+//  Copyright © 2017 Kane Stapler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
